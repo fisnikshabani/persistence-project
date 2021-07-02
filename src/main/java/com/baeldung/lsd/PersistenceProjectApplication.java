4d1fcdb4f6f011858bdbe7046da58564647e92bd
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-07-02 11:34:36 +0200

    Lesson 2: Introduction to Spring Data JPA Repositories: added IProjectRepository, ITaskRepository and IUserRepository

--- src/main/java/com/baeldung/lsd/PersistenceProjectApplication.java
+++ src/main/java/com/baeldung/lsd/PersistenceProjectApplication.java
@@ -1,7 +1,13 @@
 package com.baeldung.lsd;
 
+import com.baeldung.lsd.persistence.model.Project;
+import com.baeldung.lsd.persistence.model.Task;
+import com.baeldung.lsd.persistence.repository.IProjectRepository;
+import com.baeldung.lsd.persistence.repository.ITaskRepository;
+import com.baeldung.lsd.persistence.repository.IUserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,11 +15,22 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.util.Optional;
+
 
 @SpringBootApplication
 public class PersistenceProjectApplication implements ApplicationRunner {
 
     private static final Logger LOG = LoggerFactory.getLogger(PersistenceProjectApplication.class);
+
+    @Autowired
+    private IProjectRepository projectRepository;
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @Autowired
+    private IUserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(PersistenceProjectApplication.class, args);
@@ -22,5 +39,14 @@ public class PersistenceProjectApplication implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         LOG.info("Starting Spring Boot application");
+
+        Iterable<Project> allProjects = projectRepository.findAll();
+        LOG.info("All Projects:\n{}", allProjects);
+
+        Optional<Task> project1 = taskRepository.findById(1L);
+        LOG.info("Task by id 1: \n{}", project1);
+
+        long noOfUsers = userRepository.count();
+        LOG.info("Number of users: \n{}", noOfUsers);
     }
 }
---
+++ src/main/java/com/baeldung/lsd/persistence/repository/IUserRepository.java
@@ -0,0 +1,7 @@
+package com.baeldung.lsd.persistence.repository;
+
+import com.baeldung.lsd.persistence.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IUserRepository extends CrudRepository<User, Long> {
+}
