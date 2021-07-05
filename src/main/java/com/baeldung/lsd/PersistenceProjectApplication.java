cbda7a5493450a4ef514594c684afbe068f31ac4
Fisnik Shabani <fisnik.shabani@gmail.com>
2021-07-05 15:08:26 +0200

    The Page Interface, Paginating With Derived Methods Using Page, The Slice Interface, Paginating With Derived Methods Using Slice, Paginating With Custom Queries

--- src/main/java/com/baeldung/lsd/PersistenceProjectApplication.java
+++ src/main/java/com/baeldung/lsd/PersistenceProjectApplication.java
@@ -1,6 +1,7 @@
 package com.baeldung.lsd;
 
 import com.baeldung.lsd.persistence.model.Task;
+import com.baeldung.lsd.persistence.model.TaskStatus;
 import com.baeldung.lsd.persistence.repository.ITaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +10,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 
 
 @SpringBootApplication
@@ -51,6 +49,28 @@ public class PersistenceProjectApplication implements ApplicationRunner {
         Page<Task> tasksPage1SortedByNameDsc = taskRepository.findAll(tasksFirstPageSortedByNameDesc);
         LOG.info("Page 1 of all tasks sorted by name in descending order: ");
         tasksPage1SortedByNameDsc.forEach(task -> LOG.info(task.toString()));
+
+
+        Pageable twoTasksPagination = PageRequest.of(0,2);
+        Page<Task> twoTasksAPage = taskRepository.findByStatus(TaskStatus.TO_DO, twoTasksPagination);
+
+        LOG.info("All tasks by status paginated:\n {}", twoTasksAPage.getContent());
+        LOG.info("Total No of Tasks:\n{}", twoTasksAPage.getTotalElements());
+        LOG.info("Total pages of tasks: \n{}", twoTasksAPage.getTotalPages());
+
+        System.out.println("********************** Paginating With Derived Methods Using Slice *********************");
+
+        Pageable twoTasksPagination2 = PageRequest.of(0,2);
+        Slice<Task> twoTasksASlice = taskRepository.findByNameLike("Task", twoTasksPagination2);
+        LOG.info("All Tasks Sliced :\n {}", twoTasksASlice.getContent());
+        LOG.info("Are there Slices Prior:\n {}", twoTasksASlice.hasPrevious());
+        LOG.info("Are there Slices After:\n {}", twoTasksASlice.hasNext());
+
+        System.out.println("********************** Paginating With Custom Queries *********************");
+
+        Pageable pageTwo = PageRequest.of(1, 2);
+        Page<Task> allTasksByNamePageTwo = taskRepository.allTasksByName("Task%", pageTwo);
+        LOG.info("Page Two of All Tasks By Name:\n {}", allTasksByNamePageTwo.getContent());
 
     }
 }
--- src/main/java/com/baeldung/lsd/persistence/repository/ITaskRepository.java
+++ src/main/java/com/baeldung/lsd/persistence/repository/ITaskRepository.java
@@ -1,6 +1,10 @@
 package com.baeldung.lsd.persistence.repository;
 
 import com.baeldung.lsd.persistence.model.Task;
+import com.baeldung.lsd.persistence.model.TaskStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -10,5 +14,12 @@ public interface ITaskRepository extends PagingAndSortingRepository<Task, Long> 
     @Modifying
     @Query("delete Task t where t.status = com.baeldung.lsd.persistence.model.TaskStatus.DONE")
     int deleteCompletedTasks(); //modifying queries can return either void, int, or even Integer. However, returning anything other than these would result in an IllegalArgumentException.
+
+    Page<Task> findByStatus(TaskStatus status, Pageable pageable);
+
+    Slice<Task> findByNameLike (String name, Pageable pageable);
+
+    @Query("select t from Task t where t.name like ?1")
+    Page<Task> allTasksByName(String name, Pageable pageable);
 
 }
